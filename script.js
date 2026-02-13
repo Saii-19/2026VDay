73a6e03ebb34e8f370c0029cd7b8317cef535a43
Saii-19 <poondlasaikiran2000@gmail.com>
2026-02-14 00:18:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const noBtn = document.getElementById("noBtn");
 
 if (noBtn) {
-    noBtn.addEventListener("mouseover", function() {
-        const x = Math.random() * (window.innerWidth - 100);
-        const y = Math.random() * (window.innerHeight - 50);
-        noBtn.style.left = x + "px";
-        noBtn.style.top = y + "px";
+    noBtn.addEventListener("mouseenter", function () {
+        const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+        const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+        noBtn.style.transform = `translate(${x}px, ${y}px)`;
     });
 }
