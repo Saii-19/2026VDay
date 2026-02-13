6a2c93fbf62b02c6fed8794831abfa88f8d901a7
Saii-19 <poondlasaikiran2000@gmail.com>
2026-02-13 23:58:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const noBtn = document.getElementById("noBtn");
+
+if (noBtn) {
+    noBtn.addEventListener("mouseover", function() {
+        const x = Math.random() * (window.innerWidth - 100);
+        const y = Math.random() * (window.innerHeight - 50);
+        noBtn.style.left = x + "px";
+        noBtn.style.top = y + "px";
+    });
+}
